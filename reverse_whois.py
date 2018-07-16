b41506e21dee0ec1cf6462c3cf7942dc4fdc827c
Tony Rivera <tonyrivera@users.noreply.github.com>
2018-07-16 11:45:18 -0700

    Remove import csv

--- reverse_whois.py
+++ reverse_whois.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import requests
 from bs4 import BeautifulSoup
-import csv
 
 url = 'https://viewdns.info/reversewhois/?q=' # Scrape reverse whois info from here
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
