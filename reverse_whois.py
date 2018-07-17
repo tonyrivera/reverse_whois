f36d9d68360b64225a2b30d36197ea9f36376932
Tony Rivera <tonyrivera@users.noreply.github.com>
2018-07-16 18:25:32 -0700

    Replace input with cmdline interface argparse

--- reverse_whois.py
+++ reverse_whois.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import requests
 from bs4 import BeautifulSoup
+import argparse
 
+# Config
 url = 'https://viewdns.info/reversewhois/?q=' # Scrape reverse whois info from here
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
-query = input('Enter Registrant Name or Email Address: ')
 
-r = requests.get(url + query, headers = headers)
+# Command Line Interface
+parser = argparse.ArgumentParser(description = 'Reverse whois from registrant name or email address.')
+parser.add_argument("-i", "--input", help = 'enter an email or full name', type = str, required = True)
+args = parser.parse_args()
+
+r = requests.get(url + args.input, headers = headers)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
 for row in soup('table')[3].find_all('tr'):
-    print( row.find_all('td')[0].text )
+    print(row.find_all('td')[0].text)
