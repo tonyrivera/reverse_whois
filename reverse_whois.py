da8a0cd6396442c8a3804a14eb86a7a21d3649ca
Tony Rivera <tonyrivera@users.noreply.github.com>
2018-07-16 19:32:08 -0700

    Move comments

--- reverse_whois.py
+++ reverse_whois.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 import argparse
 
 # Config
-url = 'https://viewdns.info/reversewhois/?q=' # Scrape reverse whois info from here
+# URL to scrape reverse whois data
+url = 'https://viewdns.info/reversewhois/?q='
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
 
 # Command Line Interface
